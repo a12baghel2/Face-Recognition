8a551a1dafc465afd2785780827c48149bd05af7
Sourav singh <56843128+srvsingh1962@users.noreply.github.com>
2020-10-01 11:32:40 +0530

    Adding comments into the code.

--- face_recognition.py
+++ face_recognition.py
@@ -1,85 +1,60 @@
 import cv2
 import numpy as np 
-import os
-
-def distance(v1,v2):
-	return np.sqrt(((v1-v2)**2).sum())
-
-def knn(train,test,k=5):
-	dist = []
-	for i in range(train.shape[0]):
-		ix = train[i,:-1]
-		iy = train[i,-1]
-		d = distance(test,ix)
-		dist.append([d,iy])
-
-	dk = sorted(dist,key=lambda x:x[0])[:k]
-	labels = np.array(dk)[:,-1]
-
-	output = np.unique(labels,return_counts= True)
-	index = np.argmax(output[1])
-	return output[0][index]
 
 cap = cv2.VideoCapture(0)
 
+#Load the haarcascade classifier file
 face_cascade = cv2.CascadeClassifier("haarcascade_frontalface_alt.xml")
 
 skip = 0
-dataset_path = "./"
 face_data = []
-labels = []
+data_set = "./"
 
-class_id = 0
-names = {}
+#Enter the filename or the name of person you are collecting the data 
+file_name = input("Enter the File Name: ")
 
-for fx in os.listdir(dataset_path):
-	if fx.endswith('.npy'):
-		names[class_id] = fx[:-4]
-		print("loaded "+fx)
-		data_item = np.load(dataset_path+fx)
-		face_data.append(data_item)
-		target = class_id*np.ones((data_item.shape[0],))
-		class_id += 1
-		labels.append(target)
-
-
-face_dataset = np.concatenate(face_data,axis=0)
-face_labels = np.concatenate(labels,axis= 0).reshape((-1,1))
-
-print(face_dataset.shape)
-print(face_labels.shape)
-
-trainset = np.concatenate((face_dataset,face_labels),axis = 1)
-
-print(trainset.shape)
+#main
 
 while True:
 	ret,frame = cap.read()
+	#gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 	if ret == False:
 		continue
-
 	faces = face_cascade.detectMultiScale(frame,1.3,5)
-	if(len(faces)==0):
-		continue
+	faces = sorted(faces,key= lambda f:f[2]*f[3])
 
-	for face in faces:
-		x,y,w,h = face
+	#Drawing Rectangle around the Face
+	for (x,y,w,h) in faces[-1:]:
+		cv2.rectangle(frame,(x,y),(x+w,y+h),(0,230,255),2)
 
 		offset = 10
 		face_section = frame[y-offset:y+h+offset,x-offset:x+w+offset]
 		face_section = cv2.resize(face_section,(100,100))
 
-		out = knn(trainset,face_section.flatten())
+		skip += 1
+		if skip%10 == 0:
+			face_data.append(face_section)
+			print(len(face_data))
 
-		pred_name = names[int(out)]
+	#cv2.imshow("face section",face_section)
 
-		cv2.putText(frame,pred_name,(x,y-10),cv2.FONT_HERSHEY_SIMPLEX,1,(255,0,0),2,cv2.LINE_AA)
-		cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,230),2)
+	#Showing the image
+	cv2.imshow("large_frame",frame)
 
-	cv2.imshow("faces",frame)
-	key = cv2.waitKey(1) & 0xFF
-	if key == ord('q'):
+	#Exit condition. When 'q' is pressed
+	key_pressed = cv2.waitKey(1) & 0xFF
+	if key_pressed == ord('q'):
 		break
+
+#creating the data array that stores the data 
+face_data = np.asarray(face_data)
+face_data = face_data.reshape((face_data.shape[0],-1))
+print(face_data.shape)
+
+#saving in npy file in the directory
+np.save(data_set+file_name+".npy",face_data)
+print("data successfully saved")
+
 
 cap.release()
 cv2.destroyAllWindows()
